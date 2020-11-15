47f418a895110cf7b03543ffe0428224274af04c
Troy Hart <thart@myriad.com>
2020-11-14 18:04:31 -0700

    chore: initial commit

---
+++ src/main/java/com/example/jx3demoapp5/Jx3Demoapp5Application.java
@@ -0,0 +1,27 @@
+package com.example.jx3demoapp5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+@RequestMapping(path = "/hello")
+public class Jx3Demoapp5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jx3Demoapp5Application.class, args);
+    }
+
+    @GetMapping
+    public Map<String, String> hello() {
+        return Map.of("hello","world");
+    }
+
+}
---
+++ src/test/java/com/example/jx3demoapp5/Jx3Demoapp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jx3demoapp5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jx3Demoapp5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
