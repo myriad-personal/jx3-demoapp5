7276fad327e3241d9350c57b1ff1253460546bdb
Troy Hart <thart@myriad.com>
2020-12-04 13:07:40 -0700

    fix: default path

--- src/main/java/com/example/jx3demoapp5/Jx3Demoapp5Application.java
+++ src/main/java/com/example/jx3demoapp5/Jx3Demoapp5Application.java
@@ -2,8 +2,6 @@ package com.example.jx3demoapp5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpRequest;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +10,7 @@ import java.util.Map;
 
 @SpringBootApplication
 @RestController
-@RequestMapping(path = "/hello")
+@RequestMapping(path = "/")
 public class Jx3Demoapp5Application {
 
     public static void main(String[] args) {
